a39807b80adbe0a246cc5b99468f6f6ae4a06927
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-03 18:25:30 -0500

    setting up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import Todos from './components/Todos'
 
 class App extends Component {
+
+  state = {
+    todos: [
+      {
+      title: 'Trash',
+      id: 1,
+      completed: false},
+      {
+      title: 'Dog',
+      id: 2,
+      completed: false},
+      {
+      title: 'Dinner',
+      id: 3,
+      completed: false},
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos/>
       </div>
     );
   }
